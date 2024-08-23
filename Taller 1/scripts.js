f436e39a0efb72d8d46d2c9166b01f6881758cc9
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2024-08-23 13:22:19 -0500

    Practicando js

---
+++ Taller 1/scripts.js
@@ -0,0 +1,38 @@
+// Programa para saber si un número es par o impar
+
+let entrada = document.getElementById("entradaNumero");
+let salida = document.getElementById("salidaNumero")
+
+document.getElementById("boton").addEventListener("click", function () {
+  if (entrada.value % 2 === 0) {
+    salida.value = "El número " + entrada.value + " es par";
+    // entrada.value = ''
+  } else {
+    salida.value = "El número es impar";
+  }
+  entrada.value = ''
+})
+
+
+// Programa para saber si una letra es vocal o consonante, mayúscula o minúscula
+
+let entradaLetra = document.getElementById("entradaLetra");
+let salidaLetra = document.getElementById("salidaLetra")
+const vocales = ["a", "e", "i", 'o', 'u'];
+
+
+document.getElementById("botonLetra").addEventListener("click", function () {
+
+for (let letra of vocales){
+  if(entradaLetra.value == letra){
+    salidaLetra.value = `La letra "${entradaLetra.value}" es una vocal.`
+    break;
+  } else{
+    salidaLetra.value = `La letra "${entradaLetra.value}" es una consonante.`
+    break;
+  }
+}
+entradaLetra.value = ''
+
+})
+
